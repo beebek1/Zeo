c610aa9347d869de6dabaf1b0daef276c53e0768
beebek1 <beebek990@gmail.com>
2025-12-16 14:05:39 +0545

    login and signup completed

---
+++ app/src/main/java/com/example/futsal/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.example.futsal.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF000000)
+
+val Blue = Color(0xFF29559B)
+
+val Mint = Color(0xFF63CF9E)
+
+val White = Color(0xFFFFFFFF)
+
+val Green = Color(0xFF38A36A)
+
+val Lightgray = Color(0xFFF0F0F0)
+
+
