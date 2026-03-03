f11a5192e14fdfa1c5144d59778b6b5ad290e673
beebek1 <beebek990@gmail.com>
2026-03-03 17:08:53 +0545

    feat: update UserModel schema

--- app/src/main/java/com/example/zeo/model/UserModel.kt
+++ app/src/main/java/com/example/zeo/model/UserModel.kt
@@ -1,21 +1,15 @@
 package com.example.zeo.model
 
 data class UserModel(
-    var userId: String ="",
-    var email: String ="",
-    var fullName: String ="",
-
-
-){
-    fun toMap() : Map<String,Any?>{
+    var userId: String = "",
+    var email: String = "",
+    var fullName: String = "",
+) {
+    fun toMap(): Map<String, Any?> {
         return mapOf(
             "userId" to userId,
             "email" to email,
-            "firstName" to fullName
-
+            "fullName" to fullName // Corrected key to match property name
         )
     }
 }
-
-
-
